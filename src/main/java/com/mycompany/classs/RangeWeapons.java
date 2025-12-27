fba754f92978d58254494dee3fbd4d37aba695ec
Ayham133 <ayhamatallah52@gmail.com>
2025-12-27 21:20:07 +0200

    fixed the checking in the RangeWeapons.java file, now it checks the correct item id for the dealDamge method

--- src/main/java/com/mycompany/classs/RangeWeapons.java
+++ src/main/java/com/mycompany/classs/RangeWeapons.java
@@ -1,7 +1,6 @@
 package com.mycompany.classs;
 
 import com.mycompany.app.GameStates;
-import com.mycompany.classs.gameItems.GameWeapons;
 import com.mycompany.enums.Rarity;
 
 /**
@@ -9,16 +8,18 @@ import com.mycompany.enums.Rarity;
  */
 public class RangeWeapons extends Weapons {
 
-    public RangeWeapons(String name, Rarity rarity, double damage) {
+    private final Item THROWABLES;
+
+    public RangeWeapons(String name, Rarity rarity, double damage, Item THROWABLES) {
         super(name, rarity, damage);
+        this.THROWABLES = THROWABLES;
     }
 
     @Override
     public double dealDamage() {
-        if (GameStates.getPlayer().getInventory().contains(GameWeapons.SWAORD.getID()))
+        if (GameStates.getPlayer().getInventory().contains(THROWABLES.getID()))
             return 25;
         else
             return 0;
-
     }
 }
